fb302dbcf689e9cbe7aa7ff3ae71cdc70b9cb7ec
carsten orth <carsten_orth1@hotmail.de>
2016-05-15 22:03:33 +0200

    text cleaner

---
+++ src/textminingtest/TextCleaner.java
@@ -0,0 +1,40 @@
+package textminingtest;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TextCleaner {
+	public static void main(String[] args) {
+		try {
+			cleanTxt("data/SmallTest.txt", "data/cleanTest.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static void cleanTxt(String input, String output) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(input));
+		PrintWriter writer = new PrintWriter(output, "UTF-8");
+		try {
+		    String line = br.readLine();
+
+		    while (line != null) {
+		    	line = line.replaceAll("\\d","");
+		    	if(line.isEmpty()||line.equals("Artemis Fowl")||line.startsWith("CHAPTER")||line.startsWith("Chapter :")){
+		    		line = br.readLine();
+		    		continue;
+		    	}
+		    	writer.println(line);
+		        line = br.readLine();
+		    }
+		    
+		} finally {
+		    br.close();
+		}
+		writer.close();
+		System.out.println("clean");
+	}
+}
