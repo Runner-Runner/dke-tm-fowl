ae02d03784f9f24faa6422b9fb8811e236564d0e
carsten <carsten_orth1@hotmail.de>
2016-05-19 16:32:57 +0200

    multiple sentences relation

--- src/textminingtest/TextMiner.java
+++ src/textminingtest/TextMiner.java
@@ -11,142 +11,127 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
 
-public class TextMiner
-{
-  private String directory;
+public class TextMiner {
+	private String directory;
 
-  private EntityManager entityManager;
+	private EntityManager entityManager;
 
-  private HashMap<Integer, String> corefIdMapping;
+	private HashMap<Integer, String> corefIdMapping;
 
-  private static final String ANNOTATORS
-          = "tokenize, ssplit, pos, lemma, ner, regexner, parse, dcoref, sentiment";
-  
-  private static final String ENTITY_TAG = "PERSON";
+	private static final String ANNOTATORS = "tokenize, ssplit, pos, lemma, ner, regexner, parse, dcoref, sentiment";
 
-  public TextMiner()
-  {
-    entityManager = new EntityManager();
-    corefIdMapping = new HashMap<>();
-  }
+	private static final String ENTITY_TAG = "PERSON";
 
-  public void setDirectory(String directory)
-  {
-    this.directory = directory;
-  }
+	public TextMiner() {
+		entityManager = new EntityManager();
+		corefIdMapping = new HashMap<>();
+	}
 
-  public void mineText(boolean writeInFile, String directText)
-  {
-    try
-    {
-      PrintWriter out;
-      if (writeInFile)
-      {
-        out = new PrintWriter("output1.txt");
-      }
-      else
-      {
-        out = new PrintWriter(System.out);
-      }
+	public void setDirectory(String directory) {
+		this.directory = directory;
+	}
 
-      Properties props = new Properties();
-      props.setProperty("annotators", ANNOTATORS);
-      props.setProperty("regexner.mapping", "regexner.txt");
-//      props.setProperty("ner.model", "edu/stanford/nlp/models/ner/english.conll.4class.distsim.crf.ser.gz");
+	public void mineText(boolean writeInFile, String directText) {
+		try {
+			PrintWriter out;
+			if (writeInFile) {
+				out = new PrintWriter("output1.txt");
+			} else {
+				out = new PrintWriter(System.out);
+			}
 
-      StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+			Properties props = new Properties();
+			props.setProperty("annotators", ANNOTATORS);
+			props.setProperty("regexner.mapping", "regexner.txt");
+		//	props.setProperty("ner.model",
+			//		"edu/stanford/nlp/models/ner/english.all.3class.caseless.distsim.crf.ser.gz");
 
-      List<Annotation> fileAnnotations = new ArrayList<>();
-      if (directText != null)
-      {
-        fileAnnotations.add(new Annotation(directText));
-      }
-      else
-      {
-        File directoryFile = new File(directory);
-        File[] listFiles = directoryFile.listFiles();
-        for (File file : listFiles)
-        {
-          String fileText = IOUtils.slurpFileNoExceptions(file);
-          fileAnnotations.add(new Annotation(fileText));
-        }
-      }
+			StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
-      int c = 0;
-      for (Annotation annotation : fileAnnotations)
-      {
-        c++;
-        System.out.println("Processing annotation: "+c);
-        PrintWriter xmlOut = new PrintWriter("xmlOutputAnno"+c+".xml");
-        
-        //ids might change for different texts -> reset
-        corefIdMapping.clear();
-        
-        // run all the selected Annotators on this text
-        pipeline.annotate(annotation);
+			List<Annotation> fileAnnotations = new ArrayList<>();
+			if (directText != null) {
+				fileAnnotations.add(new Annotation(directText));
+			} else {
+				File directoryFile = new File(directory);
+				File[] listFiles = directoryFile.listFiles();
+				for (File file : listFiles) {
+					String fileText = IOUtils.slurpFileNoExceptions(file);
+					fileAnnotations.add(new Annotation(fileText));
+				}
+			}
+			int relationRange = 2; //(sentences)
+			int c = 0;
+			for (Annotation annotation : fileAnnotations) {
+				c++;
+				System.out.println("Processing annotation: " + c);
+				PrintWriter xmlOut = new PrintWriter("xmlOutputAnno" + c + ".xml");
 
-        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
-        if (sentences != null && !sentences.isEmpty())
-        {
-          for (CoreMap sentence : sentences)
-          {
-            List<String> personNamesInSentence = new ArrayList<>();
-            for (CoreMap token : sentence.get(CoreAnnotations.TokensAnnotation.class))
-            {
-              Integer corefClusterId = token.get(CorefClusterIdAnnotation.class);
-              String name = corefIdMapping.get(corefClusterId);
+				// ids might change for different texts -> reset
+				corefIdMapping.clear();
 
-              String namedEntityTag = token.get(NamedEntityTagAnnotation.class);
-              String word = null;
-              if (ENTITY_TAG.equals(namedEntityTag))
-              {
-                word = token.get(CoreAnnotations.TextAnnotation.class);
+				// run all the selected Annotators on this text
+				pipeline.annotate(annotation);
 
-                if (corefClusterId != null && name == null)
-                {
-                  corefIdMapping.put(corefClusterId, word);
-                }
-              }
-              else
-              {
-                word = name;
-              }
+				List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+				if (sentences != null && !sentences.isEmpty()) {
+					LinkedList<HashSet<String>> namesBefore = new LinkedList<HashSet<String>>();
+					for (CoreMap sentence : sentences) {
+						HashSet<String> personNamesInSentence = new HashSet<String>();
+						for (CoreMap token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+							Integer corefClusterId = token.get(CorefClusterIdAnnotation.class);
+							String name = corefIdMapping.get(corefClusterId);
 
-              if (word != null && !personNamesInSentence.contains(word))
-              {
-                personNamesInSentence.add(word);
-              }
+							String namedEntityTag = token.get(NamedEntityTagAnnotation.class);
+							String word = null;
+							if (ENTITY_TAG.equals(namedEntityTag)) {
+								word = token.get(CoreAnnotations.TextAnnotation.class);
 
-            }
-            for (int i = 0; i < personNamesInSentence.size(); i++)
-            {
-              String personName1 = personNamesInSentence.get(i);
-              for (int j = i + 1; j < personNamesInSentence.size(); j++)
-              {
-                String personName2 = personNamesInSentence.get(j);
+								if (corefClusterId != null && name == null) {
+									corefIdMapping.put(corefClusterId, word);
+								}
+							} else {
+								word = name;
+							}
 
-                entityManager.increaseRelation(personName1, personName2);
-              }
-            }
-          }
-        }
-        
-        pipeline.xmlPrint(annotation, xmlOut);
-        IOUtils.closeIgnoringExceptions(xmlOut);
-      }
+							if (word != null) {
+								personNamesInSentence.add(word);
+							}
 
-      IOUtils.closeIgnoringExceptions(out);
-      
-      GephiExporter.exportCSV(entityManager.getEntities().values(), "test");
-      WekaParser.entitiesToWeka(entityManager.getEntities().values(), "test");
-    }
-    catch (Exception ex)
-    {
-      System.out.println(ex.getMessage());
-    }
+						}
+						namesBefore.addFirst(personNamesInSentence);
+						if(namesBefore.size()>relationRange)
+							namesBefore.removeLast();
+						HashSet<String> alreadyChecked = new HashSet<String>();
+						for(String personOne:personNamesInSentence){
+							alreadyChecked.add(personOne);
+							for(HashSet<String> sentenceBefore: namesBefore){
+								for(String personTwo: sentenceBefore){
+									if(!alreadyChecked.contains(personTwo)){
+										entityManager.increaseRelation(personOne, personTwo);
+									}
+								}
+									
+							}
+						}
+					}
+				}
 
-  }
+				pipeline.xmlPrint(annotation, xmlOut);
+				IOUtils.closeIgnoringExceptions(xmlOut);
+			}
+
+			IOUtils.closeIgnoringExceptions(out);
+
+			GephiExporter.exportCSV(entityManager.getEntities().values(), "test");
+			WekaParser.entitiesToWeka(entityManager.getEntities().values(), "test");
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+
+	}
 }
